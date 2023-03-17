f3cf20e6d871b2778f97909e6a53e7b727986f33
YunPeng <2216848233@qq.com>
2023-03-17 21:05:07 +0800

    完善api请求函数

--- src/api/chapter.ts
+++ src/api/chapter.ts
@@ -1,6 +1,11 @@
 import {http} from "./index";
 
-export const getChapter = (bookId:string, chapterId: string)=>
+/**
+ * 获取章节信息 包含内容
+ * @param bookId 书籍id
+ * @param chapterId 章节id 没有则查询第一张
+ */
+export const getChapter = (bookId:string, chapterId?: string)=>
     http.get("/chapter/read", {params:{bookId, chapterId}});
 
 export const getIdList = (bookId:string) =>
